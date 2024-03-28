fc147e62d10b2743384943ed960eb56fec685091
Mayra <mayra203@gmail.com>
2024-03-28 08:49:26 -0600

    import httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { MoviesService } from './services/movies.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), 
+  provideHttpClient(),
+  provideClientHydration()
+]
 };
