81c4683ab15b689b9acadebdf34c1341ef9ef53c
Mayra <mayra203@gmail.com>
2024-03-28 08:49:02 -0600

    Created movie model

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,16 @@
+export interface Movie {
+    adult:             boolean;
+    backdrop_path:     string;
+    genre_ids:         number[];
+    id:                number;
+    origin_country:    string[];
+    original_language: string;
+    original_name:     string;
+    overview:          string;
+    popularity:        number;
+    poster_path:       string;
+    first_air_date:    Date;
+    title:              string;
+    vote_average:      number;
+    vote_count:        number;
+}
