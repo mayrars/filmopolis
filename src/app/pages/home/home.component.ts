86b7e6697c0012e7b624d64f2391a4f145ddd5bb
Mayra <mayra203@gmail.com>
2024-03-29 06:29:28 -0600

    Get Popular movie and change header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,18 +2,21 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { AsyncPipe } from '@angular/common';
 import { Movie } from '../../models/movie.model';
+import { environment } from '../../../environments/environment';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   private _moviesService = inject(MoviesService);
   topMovies: Movie[] = [];
+  urlImg: string = environment.imgUrl
   ngOnInit(): void {
-    this._moviesService.getTopMovies().subscribe(data=>{
+    this._moviesService.getPopularMovies().subscribe(data=>{
       this.topMovies = data.results;
     })
   }
