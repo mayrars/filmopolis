6f4751c621ec17f7826bf9d7d56b55ea5bd72b03
Mayra <mayra203@gmail.com>
2024-04-01 06:58:57 -0600

    Created services for getmovies, categories and trending movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -10,12 +10,42 @@ export class MoviesService {
   private http = inject(HttpClient)
   private baseUrl = environment.apiUrl
   constructor() {}
-  getPopularMovies():Observable<any> {
+  //Trending series and movies
+  getTrendingNow():Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders()
       .set('Content-Type', 'application/json')
       .set('Authorization',`Bearer ${environment.apiKey}`)
     };
-    return this.http.get(`${this.baseUrl}movie/popular`,httpOptions)
+    return this.http.get(`${this.baseUrl}trending/all/week`,httpOptions)
+  }
+  
+  //Trendig Movies
+  getTrendingMovies():Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization',`Bearer ${environment.apiKey}`)
+    }
+    return this.http.get(`${this.baseUrl}trending/movie/day`,httpOptions)
+  }
+
+  //Get Movies
+  getMovies():Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization',`Bearer ${environment.apiKey}`)
+    }
+    return this.http.get(`{this.baseUrl}movie/popular`, httpOptions)
+  }
+  //Get categories
+  getCategories():Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization',`Bearer ${environment.apiKey}`)
+    }
+    return this.http.get(`${this.baseUrl}genre/movie/list`, httpOptions)
   }
 }
