83f9868c58991c6bead461d02da7f6bfdaf9a7a9
Mayra <mayra203@gmail.com>
2024-04-02 06:53:26 -0600

    include binding in components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { MoviesService } from './services/movies.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), 
-  provideHttpClient(),
-  provideClientHydration()
-]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()), 
+    provideHttpClient(),
+    provideClientHydration()
+  ]
 };
