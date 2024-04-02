f8c64d760068952ec5237610e18d285a58316cc1
Mayra <mayra203@gmail.com>
2024-04-02 06:52:18 -0600

    import pagination and add in html template

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Movie } from '../../models/movie.model';
+import { environment } from '../../../environments/environment';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [PaginationComponent],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
-
+export class CategoryComponent implements OnInit{
+  moviesList:Movie[] = [];
+  category:string = '';
+  totalPages?: number;
+  currentPage:number=0;
+  itemsPerPage:number=20;
+  totalResults: number = 0;
+  urlImg: string = environment.imgUrl
+  private _moviesService = inject(MoviesService);
+  private _router = inject(ActivatedRoute);
+  ngOnInit(): void {
+    this._router.params.subscribe(params => {
+      this._moviesService.getMovies(params['id']).subscribe(data=>{
+        this.currentPage = data.page;
+        this.totalPages = data.total_pages;
+        this.totalResults = data.total_results;
+        console.log(this.totalResults);
+        this.moviesList = data.results;
+      })
+    })
+  }
 }
