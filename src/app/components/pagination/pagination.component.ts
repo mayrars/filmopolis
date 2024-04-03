6b2d8f6f12595be27c53d7a61549801f3335dc9c
Mayra <mayra203@gmail.com>
2024-04-03 06:59:01 -0600

    Mofied pagination

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css'
 })
@@ -11,14 +12,19 @@ export class PaginationComponent implements OnInit{
   @Input() totalItems: any;
   @Input() currentPage: any;
   @Input() itemsPerPage: any;
+  @Input() categoryId: any;
+  @Output() OnClick: EventEmitter<number> = new EventEmitter();
 
   totalPages = 0
   pages:any = []
   ngOnInit(): void {
-    console.log("Total",this.totalItems)
     if(this.totalItems) {
       this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage)
       this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1)
     }
+  }
+  pageClicked(page: number){
+    if(page<=this.totalPages && page>=1)
+      this.OnClick.emit(page)
   }
 }
