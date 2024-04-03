2c56d93e7035e89f7ce2e87e8b4c5dc699276783
Mayra <mayra203@gmail.com>
2024-04-03 06:58:40 -0600

    adding new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { MovieComponent } from './pages/movie/movie.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
+    {path:"categories",component:CategoriesComponent},
     {path:"category/:id",component:CategoryComponent},
+    {path:"movie/:id",component:MovieComponent},
     {path:"**",component:HomeComponent},
 ];
