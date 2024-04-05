8d5a4073773dbd63c4848ba2a6b29dce1200f122
Mayra <mayra203@gmail.com>
2024-04-05 06:55:20 -0600

    Import card component and adding pupular movies

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,24 +4,25 @@ import { AsyncPipe } from '@angular/common';
 import { Movie } from '../../models/movie.model';
 import { environment } from '../../../environments/environment';
 import { RouterLink } from '@angular/router';
+import { CardComponent } from '../../components/card/card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe,RouterLink],
+  imports: [AsyncPipe,RouterLink,CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   private _moviesService = inject(MoviesService);
   topTrending: Movie[] = [];
-  topMovies: Movie[] = [];
+  popularMovies: Movie[] = [];
   urlImg: string = environment.imgUrl
   ngOnInit(): void {
-    this._moviesService.getTrendingMovies().subscribe(data=>{
-      this.topMovies = data.results;
+    this._moviesService.getPopularMovies().subscribe((data)=>{
+      this.popularMovies = data.results.slice(0,8)
     })
     this._moviesService.getTrendingNow().subscribe(data=>{
-      this.topTrending = data.results;
+      this.topTrending = data.results.slice(0,8)
     })
   }
 }
