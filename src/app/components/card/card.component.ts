f716d25f83babd30ca40c31547c1af8117fee0ae
Mayra <mayra203@gmail.com>
2024-04-05 06:54:29 -0600

    Move content to card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie.model';
+import { environment } from '../../../environments/environment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
-
+export class CardComponent implements OnInit{
+  @Input() movie?: Movie;
+  urlImg: string = environment.imgUrl
+  ngOnInit(): void {
+  }
 }
