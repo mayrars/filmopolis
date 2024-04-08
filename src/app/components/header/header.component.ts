12080b230a87d62d634c2a196a2f9283a19cd385
Mayra <mayra203@gmail.com>
2024-04-08 06:51:29 -0600

    Get tvshows

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Categorie } from '../../models/category.model';
 import { RouterLink } from '@angular/router';
+import { TvshowsService } from '../../services/tvshows.service';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +13,15 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent implements OnInit{
   private _moviesService = inject(MoviesService);
+  private _tvshowsService = inject(TvshowsService);
   categories: Categorie[] = [];
+  tvcategories: Categorie[] = [];
   ngOnInit(): void {
     this._moviesService.getCategories().subscribe(data=>{
       this.categories = data.genres
+    })
+    this._tvshowsService.getCategories().subscribe(data=>{
+      this.tvcategories = data.genres
     })
   }
 }
