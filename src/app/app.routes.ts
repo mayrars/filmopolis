97f79cb9332d6bdf59b2b759e71bfa0b6e157ff7
Mayra <mayra203@gmail.com>
2024-04-08 06:52:10 -0600

    Created routes for tv shows

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { MovieComponent } from './pages/movie/movie.component';
+import { TvshowComponent } from './pages/tvshow/tvshow.component';
+import { TvshowCategoryComponent } from './pages/tvshow-category/tvshow-category.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"categories",component:CategoriesComponent},
     {path:"category/:id",component:CategoryComponent},
+    {path:"categoriestv/:id",component:TvshowCategoryComponent},
     {path:"movie/:id",component:MovieComponent},
+    {path:"tvshow/:id",component:TvshowComponent},
     {path:"**",component:HomeComponent},
 ];
