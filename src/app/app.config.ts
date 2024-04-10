34d9fbabdc3956e904b8b3f4901a544570b853eb
Mayra <mayra203@gmail.com>
2024-04-10 06:58:01 -0600

    Adding with fetch in http provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()), 
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideClientHydration()
   ]
 };
