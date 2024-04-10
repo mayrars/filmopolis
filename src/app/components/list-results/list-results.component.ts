b4646dc1ddb44efae92b71b4f9cd25e05815cea9
Mayra <mayra203@gmail.com>
2024-04-10 06:57:10 -0600

    Update results

--- src/app/components/list-results/list-results.component.ts
+++ src/app/components/list-results/list-results.component.ts
@@ -1,14 +1,21 @@
 import { NgClass } from '@angular/common';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list-results',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass,RouterLink],
   templateUrl: './list-results.component.html',
   styleUrl: './list-results.component.css'
 })
 export class ListResultsComponent{
   @Input() results:any= [];
   @Input() hidden:boolean= true;
+  @Output() OnClick: EventEmitter<boolean> = new EventEmitter();
+  urlImg: string = environment.imgUrl
+  close(){
+    this.hidden = true;
+  }
 }
