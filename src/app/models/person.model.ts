90a1d667357ad34df028374fa72e56f27a70bad2
Mayra <mayra203@gmail.com>
2024-04-11 06:57:12 -0600

    Change model for person

--- src/app/models/person.model.ts
+++ src/app/models/person.model.ts
@@ -1,14 +1,17 @@
 export interface Person {
     adult:                boolean;
+    also_known_as:        string[];
+    biography:            string;
+    birthday:             Date;
+    deathday:             null;
     gender:               number;
+    homepage:             null;
     id:                   number;
+    imdb_id:              string;
     known_for_department: string;
     name:                 string;
-    original_name:        string;
+    place_of_birth:       string;
     popularity:           number;
     profile_path:         string;
-    cast_id:              number;
-    character:            string;
-    credit_id:            string;
-    order:                number;
+    character?:            string;
 }
