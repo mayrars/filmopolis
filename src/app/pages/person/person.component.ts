7c171a64675b992b53461d1cba4013dfd8b06b6c
Mayra <mayra203@gmail.com>
2024-04-11 06:56:40 -0600

    Modify person information

--- src/app/pages/person/person.component.ts
+++ src/app/pages/person/person.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Person } from '../../models/person.model';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-person',
@@ -7,6 +11,28 @@ import { Component } from '@angular/core';
   templateUrl: './person.component.html',
   styleUrl: './person.component.css'
 })
-export class PersonComponent {
+export class PersonComponent implements OnInit{
+  private _apiService = inject(ApiService);
+  private _router = inject(ActivatedRoute);
+  urlImg: string = environment.imgUrl
+  person?: Person
+  socialNetwork?: any
+  images:any
 
+  ngOnInit(): void {
+    this._router.params.subscribe(params => {
+      const id = params['id'];
+      this._apiService.getPerson(id).subscribe((person: Person) => {
+        this.person = person
+        console.log(this.person)
+      });
+      this._apiService.getSocialNetwork(id).subscribe((socialNetwork) => {
+        this.socialNetwork = socialNetwork
+      })
+      this._apiService.getPersonImages(id).subscribe((data) => {
+        this.images = data.profiles
+        console.log(this.images)
+      })
+    })
+  }
 }
